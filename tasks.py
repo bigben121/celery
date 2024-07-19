019e5c8e8d323b4f5d78752e326cd47d0d237692
bigben121 <79338079+bigben121@users.noreply.github.com>
2024-07-19 11:16:54 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -6,6 +6,12 @@ app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
 
 
+app.conf["broker_transport_options"] = {'fanout_prefix': True,
+                                                'fanout_patterns': True,
+                                                'max_connections': 2,
+                                                'socket_keepalive': True},
+app.conf["broker_pool_limit"] = 2
+
 @app.task
 def add(x, y):
     logger.info(f'Adding {x} + {y}')
